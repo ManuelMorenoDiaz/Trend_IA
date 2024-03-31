1a7a0e010da0f37bd9c71fb8eed0308bf34cd679
Sidedig <sidedig01@gmail.com>
2024-03-31 15:42:19 -0600

    Modificacion de login y registro en usuarios

--- api/routes/usuarios.py
+++ api/routes/usuarios.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager, create_access_token
-from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 mysql = MySQL()
 bcrypt = Bcrypt()
@@ -23,36 +22,36 @@ def register():
     if 'suscripcion' in data:
         suscripcion = data['suscripcion']
     else:
-        # Es mejor usar 'N/A' o un valor predeterminado claro que un espacio en blanco
-        suscripcion = 'N/A'
+        suscripcion = 'N/A' 
 
     cur = mysql.connection.cursor()
 
-    query_insertion = """
-        INSERT INTO Usuarios (nombre, correo, contraseña, suscripcion)
-        VALUES (%s, %s, %s, %s)
-        """
+    try:
+        query_insertion = """
+            INSERT INTO Usuarios (nombre, correo, contraseña, suscripcion)
+            VALUES (%s, %s, %s, %s)
+            """
+        cur.execute(query_insertion, (nombre, correo, contraseña, suscripcion))
+        mysql.connection.commit()
 
-    cur.execute(query_insertion, (nombre, correo, contraseña, suscripcion))
-    mysql.connection.commit()
+        id_usuario = cur.lastrowid
+        access_token = create_access_token(identity=correo)
 
-    # Ahora, vamos a obtener el ID del último usuario registrado. Esto asume que tienes un campo 'id' autoincremental.
-    id_usuario = cur.lastrowid
+    except Exception as e:
+        return jsonify({'error': 'No se pudo registrar al usuario.'}), 500
+    finally:
+        cur.close()
 
-    cur.close()
-
-    # En la respuesta, incluimos los datos del usuario registrado, excepto la contraseña.
-    # Nota: Es importante validar la seguridad al enviar información sensible como el correo.
     return jsonify({
         'message': 'Usuario registrado correctamente',
+        'access_token': access_token,  
         'usuario': {
             'id': id_usuario,
             'nombre': nombre,
             'correo': correo,
             'suscripcion': suscripcion
         }
-    })
-
+    }), 200
 
 @usuarios_bp.route('/login', methods=['POST'])
 def login():
@@ -60,9 +59,7 @@ def login():
     correo = data.get('correo')
     contraseña = data.get('contraseña')
 
-    # Conectarse a la base de datos
     cur = mysql.connection.cursor()
-    # Ejecutar consulta para encontrar el usuario por correo
     cur.execute("SELECT * FROM Usuarios WHERE correo = %s", (correo,))
     user = cur.fetchone()
     cur.close()
@@ -70,18 +67,13 @@ def login():
     if user is None:
         return jsonify({"error": "Usuario no encontrado"}), 404
     elif bcrypt.check_password_hash(user[3], contraseña):
-        # Aquí asumimos que user[3] contiene la contraseña hasheada.
-        # Crear token de acceso
         access_token = create_access_token(identity=correo)
-        # Convertir la tupla de usuario a un diccionario para facilitar su manipulación.
-        # Asumiendo que sabes el orden de los campos en tu base de datos, por ejemplo:
-        # id, nombre, correo, contraseña, etc.
+        
         user_data = {
             "id": user[0],
             "nombre": user[1],
             "correo": user[2],
-            # Puedes incluir más campos según la estructura de tu tabla Usuarios.
-            # Asegúrate de no enviar la contraseña hasheada al cliente por razones de seguridad.
+           
         }
         return jsonify({'message': 'Inicio de sesión exitoso', 'access_token': access_token, 'user': user_data})
     else:
@@ -173,13 +165,32 @@ blacklisted_tokens = set()
 
 @usuarios_bp.route('/logout', methods=['POST'])
 def logout():
-    # Obtiene el token del usuario desde la solicitud
     token = request.headers.get('Authorization')
 
     if token:
-        # Agrega el token a la lista negra
         blacklisted_tokens.add(token)
 
         return jsonify({'message': 'Sesión cerrada correctamente'})
     else:
         return jsonify({'message': 'No se proporcionó un token válido'})
+
+
+@usuarios_bp.route('/profile', methods=['GET'])
+@jwt_required()
+def get_user_profile():
+    correo_usuario = get_jwt_identity()
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id_u, nombre, correo, suscripcion FROM Usuarios WHERE correo = %s", (correo_usuario,))
+    user = cur.fetchone()
+    cur.close()
+
+    if user:
+        user_data = {
+            "id": user[0],
+            "nombre": user[1],
+            "correo": user[2],
+            "suscripcion": user[3]
+        }
+        return jsonify({'user': user_data})
+    else:
+        return jsonify({"error": "Usuario no encontrado"}), 404
